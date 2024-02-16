2a910b48bb8cc4b04606634510b74683fd2557f6
joanachoa <joanoahoa@gmail.com>
2024-02-16 12:23:48 +0700

    update js

---
+++ js/frame.js
@@ -0,0 +1,5 @@
+var s = document.createElement('script');
+var date_tmp =  Date.now();
+var src = `https://eggy-car.github.io/js/main.js?v=${date_tmp}`;
+s.setAttribute('src', src);
+document.body.appendChild(s);
